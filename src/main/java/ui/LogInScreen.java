cdfcc08460032c6b2e512c2a83a55fc203eb81cd
Mauricio B de Paula <mauricio.browne@gmail.com>
2018-05-09 01:00:22 -0700

    Added listener for JButton for login function.

--- src/main/java/ui/LogInScreen.java
+++ src/main/java/ui/LogInScreen.java
@@ -11,27 +11,39 @@ public class LogInScreen {
   private JFrame mainFrame;
   private JButton loginButton;
 
+  // Setting up the Frame and its contents
   private void setupGUI(){
+      // Main Frame code
       mainFrame = new JFrame("Log in");
       mainFrame.setSize(800,600);
       mainFrame.setLocationRelativeTo(null);
 
+      // Button code
       loginButton = new JButton("Login");
+      loginButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e){
+          System.out.println("Button was pressed.");
+        }
+      });
       mainFrame.add(loginButton, BorderLayout.CENTER);
 
+      //Window close listener
       mainFrame.addWindowListener(new WindowAdapter(){
         public void windowClosing(WindowEvent windowEvent){
           System.out.println("- Login was closed. Application is quitting now.");
           System.exit(0);
         }
       });
+  }
 
-      mainFrame.setVisible(true);
+  public void displayGUI(){
+    mainFrame.setVisible(true);
   }
 
   public static void main (String args[]){
     LogInScreen test = new LogInScreen();
     test.setupGUI();
+    test.displayGUI();
   }
 
 }
