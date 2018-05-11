732423cee5cc303250991f426b1d744b76eed670
Mauricio B de Paula <mauricio.browne@gmail.com>
2018-05-11 02:37:31 -0700

    Finished api service builder. Added test code to main method of login class.

--- src/main/java/LogIn.java
+++ src/main/java/LogIn.java
@@ -13,7 +13,10 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Label;
+import com.google.api.services.gmail.model.ListLabelsResponse;
 
 // Other imports
 import java.io.IOException;
@@ -33,6 +36,7 @@ public class LogIn {
   private static final String CLIENT_SECRET_DIR = "client_secret.json";
 
   private static Credential c = null;
+  private static Gmail service = null;
 
   /**
    * Creates an authorized Credential object.
@@ -56,15 +60,56 @@ public class LogIn {
   }
 
   /**
+   * Builds a new authorized API client service.
+   * @return A newly built authorized API client service.
+   * @throws IOException
+   * @throws GeneralSecurityException
+   */
+   private static Gmail buildService() throws IOException, GeneralSecurityException {
+     final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+     service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+     return service;
+   }
+
+  /**
    * Getter for the Credential object.
    * @return An authorized Credential object.
    */
    public static Credential getC(){
+     if (c == null){
+       System.out.println("Credential object was not initialized.");
+       return null;
+     }
      return c;
    }
 
-  public static void main (String args[]){
-    System.out.println("LogIn.java has been compiled and ran.");
-  }
+   /**
+    * Getter for the Gmail service object.
+    * @return An authorized API client service.
+    */
+    public static Gmail getService(){
+      if (service == null){
+        System.out.println("Gmail service object was not initialized.");
+        return null;
+      }
+      return service;
+    }
 
+  public static void main (String args[]) throws IOException, GeneralSecurityException {
+    System.out.println("LogIn.java has been compiled and ran.");
+
+    // Beginning of test code
+    buildService();
+    String user = "me";
+    ListLabelsResponse listResponse = service.users().labels().list(user).execute();
+    List<Label> labels = listResponse.getLabels();
+    System.out.println("Labels:");
+    for (Label label : labels){
+      System.out.printf("- %s\n", label.getName());
+    }
+    // End of test code
+
+  }
 }
