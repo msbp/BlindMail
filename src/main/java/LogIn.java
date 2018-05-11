5fb18d76ba4ae26e1f59778a04e1d570030999c9
Mauricio B de Paula <mauricio.browne@gmail.com>
2018-05-10 18:18:31 -0700

    Added files and initial imports for login class.

---
+++ src/main/java/LogIn.java
@@ -0,0 +1,31 @@
+/*
+* Most of the code here was adapted from google's Gmail API documentation
+*/
+
+// Google imports
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.GmailScopes;
+
+// Other imports
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+public class LogIn {
+
+  public static void main (String args[]){
+    System.out.println("Hello world.");
+  }
+}
